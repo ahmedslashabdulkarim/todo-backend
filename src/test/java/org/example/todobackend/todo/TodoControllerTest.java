3f5fbfa7de85b8b10d13cde055584b75bad09c94
ahmed <ahmedslashabdulkarim@gmail.com>
2026-03-26 16:58:30 +0100

    success TodoControllerTest

--- src/test/java/org/example/todobackend/todo/TodoControllerTest.java
+++ src/test/java/org/example/todobackend/todo/TodoControllerTest.java
@@ -131,7 +131,7 @@ class TodoControllerTest {
         // WHEN
         mockMvc.perform(get("/api/todo/888"))
                 // THEN
-                .andExpect(status().isNotFound())
+                .andExpect(status().isNotFound()) //dies kommt von @ResponseStatus(HttpStatus.NOT_FOUND) in class GlobalExceptionHandler.
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.message").value("Todo with id: 888 not found!"));
     }
